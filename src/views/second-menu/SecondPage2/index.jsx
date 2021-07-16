014ae029df5af2df9aba6e7a98d1fef1e5795751
yushu <yushu5352@163.com>
2021-07-16 14:59:02 +0800

    feat: use CheckAllBox in Page

--- src/views/second-menu/SecondPage2/index.jsx
+++ src/views/second-menu/SecondPage2/index.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import CheckAllBox from '@/components/CheckAllBox';
 
 const SecondPage2 = () => {
-  return <div>SecondPage2</div>;
+  const [data, setData] = useState([]);
+  const [defaultCheckedList, setDefaultCheckedList] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const list = ['涉气', '涉水', '噪声', '涉VOC', '危废', '固废', '放射'];
+      const defaultList = ['涉气', '涉水'];
+      setData(list);
+      setDefaultCheckedList(defaultList);
+    }, 100);
+  }, [])
+
+  const handleChange = list => {
+    console.log(list)
+  }
+
+
+  return <div>
+    {
+      (data.length && defaultCheckedList.length) ?
+        < CheckAllBox data={data} defaultCheckedList={defaultCheckedList} onChange={handleChange} />
+        :
+        []
+    }
+  </div>;
 };
 
 export default SecondPage2;
