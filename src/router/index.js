3573bd98b2cb298c2d91707eecb3546ca85673a2
YushuZhao <yushu5352@163.com>
2021-11-05 15:07:17 +0800

    feat: 实现简单的约定式路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
-import HomePage from 'views/home-menu/Map';
-import SecondPage1 from 'views/second-menu/SecondPage1';
-import ThirdPage1 from 'views/third-menu/ThirdPage1';
+// import HomePage from 'views/home-menu/Map';
+// import SecondPage1 from 'views/second-menu/SecondPage1';
+// import ThirdPage1 from 'views/third-menu/ThirdPage1';
 
 import './style.css';
 
 const routes = [];
-const routeFiles = require.context('../views', true, /\.jsx?$/); // 读取views文件夹下面所有的.jsx文件
-// console.dir(routeFiles)
+// 读取views文件夹下面所有的.jsx文件
+const routeFiles = require.context('../views', true, /\.jsx?$/);
+console.dir(routeFiles.keys());
+
 routeFiles.keys().forEach((item) => {
-  // console.log(item)
-  if (item.indexOf('component') === -1) {
-    // component下面为子组件不需要注册路由
-    let info = item.split('.');
-    // console.log(info)
+  // ./second-menu/SecondPage1/index.jsx
+  // 再次过滤，留下有index.jsx的文件路径
+  if (item.includes('/index')) {
+    let arr = item.split('.'); // ['', '/second-menu/SecondPage1/index', 'jsx']
+    let info = arr[1].replace('/index', ''); // /second-menu/SecondPage2
     routes.push({
-      path: info[1].toLowerCase(),
+      path: info.toLowerCase(),
       component: routeFiles(item).default,
     });
   }
@@ -26,23 +28,23 @@ routeFiles.keys().forEach((item) => {
 const PrimaryLayout = () => (
   <div>
     <header>
-      {/* {routes.map((route) => (
+      {routes.map((route) => (
         <Link className="layout-header" key={route.path} to={route.path}>
-          {route.path.split("/")[1]}
+          {route.path.split('/').pop()}
         </Link>
-      ))} */}
-      <Link to="/home-menu"> toHome </Link>
+      ))}
+      {/* <Link to="/home-menu"> toHome </Link>
       <Link to="/second-menu"> toSecond </Link>
-      <Link to="/third-menu"> toThird </Link>
+      <Link to="/third-menu"> toThird </Link> */}
     </header>
     <main>
       <Switch>
-        {/* {routes.map((route) => (
+        {routes.map((route) => (
           <Route exact key={route.path} {...route} />
-        ))} */}
-        <Route path="/home-menu" exact component={HomePage} />
+        ))}
+        {/* <Route path="/home-menu" exact component={HomePage} />
         <Route path="/second-menu" component={SecondPage1} />
-        <Route path="/third-menu" component={ThirdPage1} />
+        <Route path="/third-menu" component={ThirdPage1} /> */}
       </Switch>
     </main>
   </div>
