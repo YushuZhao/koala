b50157a9eab0832e5f4e7b4252a179e776241280
YushuZhao <yushu5352@163.com>
2021-11-16 16:24:23 +0800

    test: table style

---
+++ src/components/Table.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { Table } from 'antd';
+
+export default styled(Table)`
+  .ant-table, .ant-table-small{
+    .ant-table-container{
+      .ant-table-thead {
+        border-top: 1px solid transparent;
+
+        > tr {
+          > th {
+            color: #fff;
+            border-bottom: none;
+            background-color: ${({ theme }) => theme.tableHeadBg};
+          }
+
+          .ant-table-cell-scrollbar {
+            box-shadow: none;
+          }
+        }
+      }
+
+      .ant-table-tbody {
+        > tr {
+          > td {
+            color: ${({ theme }) => theme.color};;
+            border-bottom: 1px solid ${({ theme }) => theme.tableTdBorderBottom};
+            padding: 8px 8px; //列表行间距
+          }
+        }
+
+        > tr.ant-table-row:hover > td,
+        > tr.ant-table-placeholder:hover > td {
+          background: ${({ theme }) => theme.TableTdHover};
+        }
+
+        tr:nth-child(odd) {
+          background: ${({ theme }) => theme.tableTrOddBg};
+        }
+
+        tr:nth-child(even) {
+          background: ${({ theme }) => theme.tableTrEvenBg};
+        }
+
+        .ant-table-cell.ant-table-column-sort {
+          background: transparent;
+        }
+      }
+    }
+  }
+`;
--- src/style/theme.js
+++ src/style/theme.js
@@ -1,11 +1,21 @@
 export const lightTheme = {
-  background: '#fff',
-  color: '#1d1f28',
-  buttonBg: '#c5718d',
+  background: 'rgba(255, 255, 255, .1)',
+  color: '#000',
+
+  tableHeadBg: '#6793FC',
+  tableTdBorderBottom: 'rgba(255,255,255,.2)',
+  tableTrEvenBg: '#e5edf5',
+  tableTrOddBg: 'rgba(255,255,255,.2)',
+  TableTdHover: '#e6f7ff'
 };
 
 export const darkTheme = {
-  background: '#1d1f28',
-  color: '#fafafa',
-  buttonBg: '#515d90',
+  background: '#00182f',
+  color: '#fff',
+
+  tableHeadBg: '#1a3044',
+  tableTdBorderBottom: 'rgba(0,0,0,.2)',
+  tableTrEvenBg: '#011121',
+  tableTrOddBg: '#00182f',
+  TableTdHover: '#4d5e6e'
 };
